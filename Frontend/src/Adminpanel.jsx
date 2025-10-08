e5f2f79e2c275a3c984a9cd2b40de6e2650031d1
GG-GLITCH1 <officialgghackersinfo@gmail.com>
2025-10-08 18:31:59 +0100

    Create Adminpanel.jsx

---
+++ Frontend/src/Adminpanel.jsx
@@ -0,0 +1,8 @@
+export default function AdminPanel() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>👑 Admin Dashboard</h1>
+      <p>Welcome, Admin! You have full access.</p>
+    </div>
+  );
+}
